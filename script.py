118faea8be4d4713a048eef1be1a5f94c8cc0a2d
Tolik pwmz <pal3noa@yandex.ru>
2024-04-24 23:37:33 +0300

    fix problems in class

--- script.py
+++ script.py
@@ -1,7 +1,17 @@
 class Data:
     def __init__(self,data,nem ):
-        self.data = 'C:\\games\\{self.data}'
+        self.data = data
         self.nem = nem
+
+    def __enter__(self):
+        self.log_file = open(nem, 'w')
+        return self
+
+    def write(self):
+        self.log_file.write(data)
+
+    def __exit__(self):
+        self.log_file.close()
 
     def __str__(self):
         return ({self.data},{self.nem})
@@ -9,8 +19,12 @@ class Data:
 try:
     with open('C:\\games\\q.txt','r') as f:
         data = f.read()
-        nem = data.split()[0].strip()
+        nem ='C:\\games\\' + data.split()[0].strip() +'.txt'
         pt=Data(data,nem)
 except:
     print('f')
+
 print(pt.__str__())
+pt.__enter__()
+pt.write()
+pt.__exit__()
