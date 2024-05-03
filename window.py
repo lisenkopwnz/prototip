3d4b6cde4796b02de9955ac4ff0c9cde1294c805
Tolik pwmz <pal3noa@yandex.ru>
2024-05-03 15:17:26 +0300

    Добавлено однострочное текстовое поле и дополнительные функции

--- window.py
+++ window.py
@@ -1,10 +1,27 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from script import *
 
 win = tk.Tk()
 win.title('Проводник')
 win.geometry('500x500')
+icon = PhotoImage(file = "images/png-tjpeg.jpg")
+win.iconphoto(False, icon)
+
+def q():
+    try:
+        data = entry.get()
+        nem ='C:\\games\\' + data.split()[0].strip() +'.txt'
+        pt=Data(data,nem)
+        print(data)
+    except:
+        print('f')
+
+def clear():
+    entry.delete(0, END)
+
+
 
 # Создаем набор вкладок
 notebook = ttk.Notebook()
@@ -21,8 +38,11 @@ frame2.pack(fill=BOTH, expand=True)
 notebook.add(frame1, text='Записать')
 notebook.add(frame2, text='Прочитать')
 
+btn1 = ttk.Button(frame1 ,text='Отправить', command=lambda: [q(), clear()])
+btn1.pack(fill = BOTH)
 
-
+entry = ttk.Entry(frame1)
+entry.pack(fill = BOTH)
 
 
 
