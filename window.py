6c45db7969e068d63228a6a8fe837190c3bb3329
Tolik pwmz <pal3noa@yandex.ru>
2024-05-05 12:51:11 +0300

    Реализована вкладка Добавить

--- window.py
+++ window.py
@@ -10,14 +10,14 @@ win.geometry('500x500')
 icon = PhotoImage(file = "images/png-tjpeg.jpg")
 win.iconphoto(False, icon)
 
-def q():
-    try:
-        data = entry.get()
-        nem ='C:\\games\\' + data.split()[0].strip() +'.txt'
-        pt=Data(data,nem)
-        print(data)
-    except:
-        print('f')
+#def q():
+    #try:
+        #data = entry.get()
+        #nem ='C:\\games\\' + data.split()[0].strip() +'.txt'
+       # pt=Data(data,nem)
+        #print(data)
+    #except:
+        #print('f')
 
 def clear():
     entry.delete(0, END)
@@ -25,15 +25,18 @@ def clear():
 def qq():
     #os.chdir('C:\\games')
     name = entry_add.get()
-    if not (f'{name}.txt'):
+    if not (f'{name}.txt') in os.getcwd():
         open(f"{name}.txt", "w")
+        new_fail = f'{entry_add.get()}.txt'
+        fail.append(new_fail)
+        fail_set.set(fail)
+
+
 
     else:
         err_label["text"] = f'Файл с именем {name}.txt уже существует'
 
 
-
-    #print(name)
 
 
 
@@ -70,6 +73,13 @@ btn1.grid(column=1, row=2)
 frame2 = ttk.Frame(notebook)
 frame2.pack(fill=BOTH, expand=True)
 
+fail = ['ss']
+
+fail_set = Variable(value=fail)
+
+fail_listbox = Listbox(frame2, listvariable=fail_set,selectmode=SINGLE)
+fail_listbox.pack(anchor=NW, fill=X, padx=5, pady=5)
+
 
 
 # добавляем фреймы в качестве вкладок
@@ -77,8 +87,8 @@ notebook.add(frame1, text='Записать')
 notebook.add(frame2, text='Прочитать')
 notebook.add(frame3, text='Ссоздать папку')
 
-btn1 = ttk.Button(frame1 ,text='Отправить', command=lambda: [q(), clear()])
-btn1.pack(fill = BOTH)
+#btn1 = ttk.Button(frame1 ,text='Отправить', command=lambda: [q(), clear()])
+#btn1.pack(fill = BOTH)
 
 entry = ttk.Entry(frame1)
 entry.pack(fill = BOTH)
